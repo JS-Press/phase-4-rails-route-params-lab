b3b7d25a33722f67b939241e156b8b490574451c
Jarad Solomon <jaradsolomon@gmail.com>
2021-11-12 14:51:35 -0600

    CA

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    if params[:name]
-      render json: Student.find_by(first_name: 'kirby')
-    else
-    render json: Student.all
-    end
+
+    students =[]
+   if params[:name]
+                 students = Student.where("first_name LIKE ? OR last_name LIKE ?",params[:name],params[:name])
+               else
+                students = Student.all
+               end    
+    render json: students
   end
+
 
   def show 
     student = Student.find(params[:id])
